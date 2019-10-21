5aef6904ed6606f8aeab867b7915608d633adb5d
higashi000 <higashi.ict@gmail.com>
2019-10-21 22:23:12 +0900

    [fix] typo

--- src/game.cpp
+++ src/game.cpp
@@ -13,10 +13,14 @@ void Game::dispField() {
     }
   }
 
+  for (int z : step(field.agentNum)) {
+    buttons[field.myAgentData[z][2]][field.myAgentData[z][1]].draw(true);
+  }
+
   font(U"自分の得点:").draw(Vec2(800, 50), Color(0, 0, 255));
   font(field.myAreaPoint + field.myTilePoint).draw(Vec2(1200, 50), Color(0, 0, 255));
   font(U"相手の得点:").draw(Vec2(800, 130), Color(255, 0, 0));
-  font(field.myAreaPoint + field.myTilePoint).draw(Vec2(1200, 130), Color(255, 0, 0));
+  font(field.rivalAreaPoint + field.rivalTilePoint).draw(Vec2(1200, 130), Color(255, 0, 0));
 
   font(U"ターン:").draw(Vec2(800, 410), Color(0, 0, 0));
   font(field.turn).draw(Vec2(1050, 410), Color(0, 0, 0));
